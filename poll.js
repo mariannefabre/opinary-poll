64ee537cd89281bae10876c0e24086581066b2b6
Marianne Fabre <marianne@Mariannes-MacBook-Pro.local>
2021-04-08 21:22:40 +0200

    Add voting feature

--- poll.js
+++ poll.js
@@ -1,19 +1,55 @@
-function initPoll(domElement, options) {
-
-    function renderPoll() {
-      const answers = options.answers
-        .map((answer, index) => Answer(answer, index))
-        .join("");
-        domElement.innerHTML = `
-      <h2>${options.question}</h2> 
-      <ul>${answers}</ul>
-  `;
+function initPoll(element, options) {
+  let state = {
+    hasVoted: false,
+  };
+  function render() {
+    if (state.hasVoted) {
+      renderVotes();
+    } else {
+      renderPoll();
     }
-    renderPoll();
   }
-  
-  function Answer(answer, id) {
-    return `<li><button data-id="${id}">${answer}</button></li>`;
+  function renderPoll() {
+    const answers = options.answers
+      .map((answer, index) => Answer(answer, index))
+      .join("");
+    element.innerHTML = `
+    <h2>${options.question}</h2> 
+    <ul>${answers}</ul>
+`;
   }
+  function renderVotes() {
+    element.innerHTML = "votes";
+    // reduce()
+  }
+  render();
+  element.addEventListener("click", (event) => {
+    if (event.target.matches("button")) {
+      vote(event.target.dataset.id);
+      state.hasVoted = true;
+      render();
+    }
+  });
+}
 
-  
+function Answer(answer, id) {
+  return `<li><button data-id="${id}">${answer}</button></li>`;
+}
+
+function vote(answerId) {
+  let current = [];
+  try {
+    try {
+      current = JSON.parse(localStorage.getItem("answers"));
+    } catch (error) {
+      console.log(error);
+    }
+    if (current === null) {
+      current = [];
+    }
+    const serializedAnswers = JSON.stringify([...current, answerId]);
+    localStorage.setItem("answers", serializedAnswers);
+  } catch (error) {
+    console.log(error);
+  }
+}
