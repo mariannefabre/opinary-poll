be91718d4edde5624efb616c349b0e84dbe350d2
mariannefabre <marianne.fabre@hotmail.fr>
2021-04-11 18:54:45 +0200

    Fix unit tests

--- test/poll_test.js
+++ test/poll_test.js
@@ -1,17 +1,63 @@
-const assert = require('assert');
-const Answer = require('../poll.js');
+const assert = require("assert");
+const poll = require("../poll.js");
 
-describe('Answer', () => {
-  it("returns a button in a list item with a value of its first argument", ()=>{
+describe("Answer", () => {
+  it("returns a button in a list item with a value of its first argument", () => {
     // Setup
-    const answer = "first option";
+    const answer = "first answer";
     const id = 1;
-    const expected = '<li><button class="answer-button" data-id="1">first option</button></li>';
+    const expected = `<li>
+                        <button class="btn-poll-answer" data-id="1">first answer</button>
+                      </li>`;
 
     // Exercise
-    const result = Answer(answer, id);
-  
+    const result = poll.Answer(answer, id);
+
     // Verify
-    assert.strictEqual(result, expected);
+    assert.strictEqual(result.replace(/\s/g, ""), expected.replace(/\s/g, ""));
   });
-})
+});
+
+describe("AnswerWithVotes", () => {
+  it("returns a list item containing a div with the answer and its number of votes", () => {
+    // Setup
+    const answer = "first answer";
+    const id = 1;
+    const voteId = 2;
+    const nbVotes = 3;
+    const expected = `<li>
+                        <div class="poll-result">
+                          <span>first answer</span>
+                          <span class="fade-in">3 votes</span>
+                        </div>
+                      </li>`;
+
+    // Exercise
+    const result = poll.AnswerWithVotes(answer, id, nbVotes, voteId);
+
+    // Verify
+    assert.strictEqual(result.replace(/\s/g, ""), expected.replace(/\s/g, ""));
+  });
+});
+
+describe("AnswerWithVotes", () => {
+  it("returns a list item containing a div with the answer, its number of votes and the 'voted' class", () => {
+    // Setup
+    const answer = "first answer";
+    const id = 1;
+    const voteId = 1;
+    const nbVotes = 3;
+    const expected = `<li>
+                        <div class="poll-result voted">
+                          <span>first answer</span>
+                          <span class="fade-in">3 votes</span>
+                        </div>
+                      </li>`;
+
+    // Exercise
+    const result = poll.AnswerWithVotes(answer, id, nbVotes, voteId);
+
+    // Verify
+    assert.strictEqual(result.replace(/\s/g, ""), expected.replace(/\s/g, ""));
+  });
+});
